0fe3bd02e214fa16e0dba94ac27e3d44bc34b771
Maxwell Muhanda <ajaybullec@gmail.com>
2021-09-23 13:27:00 +0300

    Revamped website

--- Portfolio/main/routes.py
+++ Portfolio/main/routes.py
@@ -12,18 +12,25 @@ main = Blueprint('main', __name__)
 @main.route('/', methods=['GET', 'POST'])
 def index():
     user = User.query.filter_by(first_name='Maxwell').first()
-    return render_template('index.html', user=user)
 
-
-
-@main.route('/blog')
-def blog():
     page = request.args.get('page', type=int)
     blogs = Blog.query.order_by(
         Blog.date.desc()).paginate(page=page, per_page=6)
     cats = Category.query.all()
-    return render_template('blog.html', blogs=blogs, cats=cats)
 
+    portfolio = Portfolio.query.all()
+
+    form = ContactForm()
+
+    if form.validate_on_submit():
+        new_message = Contact(name=form.name.data,
+                               email=form.email.data, message=form.message.data)
+        db.session.add(new_message)
+        db.session.commit()
+        flash('Message Has been recieved I will get Back to you', "success")
+        return redirect(url_for('main.index'))
+
+    return render_template('index.html', user=user , blogs=blogs, cats=cats, portfolio=portfolio, form=form)
 
 
 @main.route('/blog/<string:slug>')
@@ -46,33 +53,15 @@ def category(slug):
     cats = Category.query.all()
     return render_template('category.html', category=category, cats=cats, blogs=blogs, posts=posts)
 
-
-@main.route('/work')
-def work():
-    portfolio = Portfolio.query.all()
-    return render_template('work.html', portfolio=portfolio)
-
-
-@main.route('/contact', methods=['GET', 'POST'])
-def contact():
-    form = ContactForm()
-
-    if form.validate_on_submit():
-        new_message = Contact(name=form.name.data,
-                               email=form.email.data, message=form.message.data)
-        db.session.add(new_message)
-        db.session.commit()
-        flash('Message Has been recieved I will get Back to you', "success")
-        return redirect(url_for('main.contact'))
-    
-
-    return render_template('contact.html', form=form)
-
-
-
 @main.route('/download')
 def download():
     path = os.path.join(current_app.root_path, "static/document/mypdf.pdf")
     return send_file(path, as_attachment=True)
 
+# HTML SITEMAP
+@main.route('/sitemap')
+def sitemap():
+    posts = Blog.query.all()
+    cats = Category.query.all()
+    return render_template('sitemap.html', posts=posts, cats=cats)
 
